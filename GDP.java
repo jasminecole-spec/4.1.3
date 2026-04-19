d1ef1aa4d0f491d55d4ea25e5b9e74736a9afa80
Jasmine <jasmine.cole@crpusd.org>
2026-04-18 22:46:08 -0700

    done i think

--- GDP.java
+++ GDP.java
@@ -11,7 +11,7 @@ public class GDP
 {
     public static void main(String[] args) throws IOException
     {
-        ArrayList<GDPData> dataset = GDPData.createDataSet("gdp.csv");
+        ArrayList<GDPData> dataset = GDPData.createDataSet("gdp_per_capita.csv");
         for (GDPData d : dataset)
         {
             System.out.println(d);
@@ -44,5 +44,47 @@ public class GDP
             }
         }
         System.out.println(country + " : " + maxVal);
+
+        double growth = Integer.MIN_VALUE;
+        String countryGrow = null;
+        for (int i = 0; i < dataset.size(); i++)
+        {
+             GDPData temp = dataset.get(i);
+             double last = temp.getGdpLast();
+             double first = temp.getGdpFirst();
+             double difference = last - first;
+             if(difference < 0)
+             {
+                difference = Math.abs(difference);
+             }
+                if (difference > growth)
+                {
+                    growth = difference;
+                    countryGrow = temp.getCountry();
+                }
+        }
+        System.out.println(countryGrow + " : " + growth);
+
+        System.out.println();
+        System.out.println("GDP PER CAPITA GROWTH >= US"); 
+        double differenceUS = 0.0;
+        for (GDPData temp : dataset) 
+        {
+            if (temp.getCountry().equals("United States")) 
+            {
+                differenceUS = Math.abs(temp.getGdpLast() - temp.getGdpFirst());
+                break;
+            }
+        }
+
+        for (GDPData temp : dataset) 
+        {
+            double difference = Math.abs(temp.getGdpLast() - temp.getGdpFirst());
+
+            if (difference >= differenceUS) 
+            {
+                System.out.println(temp.getCountry() + ": " + difference);
+            }
+        }
     }
 }
